06166283fd91bf4817aea4393e3e302f7984f87d
ChenWes <chenxuhua0530@163.com>
2016-12-14 14:32:50 +0800

    project init

---
+++ ArasSizeRangeTemplate/FrmMain.Designer.cs
@@ -0,0 +1,157 @@
+﻿namespace ArasSizeRangeTemplate
+{
+    partial class FrmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pan_Result = new System.Windows.Forms.Panel();
+            this.txt_AML = new System.Windows.Forms.TextBox();
+            this.pan_Collar = new System.Windows.Forms.Panel();
+            this.pan_SizeRangeType = new System.Windows.Forms.Panel();
+            this.radio_CollarSleeve = new System.Windows.Forms.RadioButton();
+            this.radio_Collar = new System.Windows.Forms.RadioButton();
+            this.btn_Start = new System.Windows.Forms.Button();
+            this.btn_Restart = new System.Windows.Forms.Button();
+            this.pan_Result.SuspendLayout();
+            this.pan_SizeRangeType.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pan_Result
+            // 
+            this.pan_Result.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pan_Result.Controls.Add(this.txt_AML);
+            this.pan_Result.Location = new System.Drawing.Point(12, 326);
+            this.pan_Result.Name = "pan_Result";
+            this.pan_Result.Size = new System.Drawing.Size(634, 174);
+            this.pan_Result.TabIndex = 0;
+            // 
+            // txt_AML
+            // 
+            this.txt_AML.Location = new System.Drawing.Point(3, 6);
+            this.txt_AML.Multiline = true;
+            this.txt_AML.Name = "txt_AML";
+            this.txt_AML.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.txt_AML.Size = new System.Drawing.Size(624, 156);
+            this.txt_AML.TabIndex = 3;
+            this.txt_AML.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txt_AML_KeyPress);
+            // 
+            // pan_Collar
+            // 
+            this.pan_Collar.AutoScroll = true;
+            this.pan_Collar.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pan_Collar.Location = new System.Drawing.Point(12, 70);
+            this.pan_Collar.Name = "pan_Collar";
+            this.pan_Collar.Size = new System.Drawing.Size(633, 249);
+            this.pan_Collar.TabIndex = 1;
+            // 
+            // pan_SizeRangeType
+            // 
+            this.pan_SizeRangeType.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pan_SizeRangeType.Controls.Add(this.radio_CollarSleeve);
+            this.pan_SizeRangeType.Controls.Add(this.radio_Collar);
+            this.pan_SizeRangeType.Controls.Add(this.btn_Start);
+            this.pan_SizeRangeType.Controls.Add(this.btn_Restart);
+            this.pan_SizeRangeType.Location = new System.Drawing.Point(12, 12);
+            this.pan_SizeRangeType.Name = "pan_SizeRangeType";
+            this.pan_SizeRangeType.Size = new System.Drawing.Size(633, 52);
+            this.pan_SizeRangeType.TabIndex = 2;
+            // 
+            // radio_CollarSleeve
+            // 
+            this.radio_CollarSleeve.AutoSize = true;
+            this.radio_CollarSleeve.Checked = true;
+            this.radio_CollarSleeve.Location = new System.Drawing.Point(105, 15);
+            this.radio_CollarSleeve.Name = "radio_CollarSleeve";
+            this.radio_CollarSleeve.Size = new System.Drawing.Size(101, 16);
+            this.radio_CollarSleeve.TabIndex = 1;
+            this.radio_CollarSleeve.TabStop = true;
+            this.radio_CollarSleeve.Text = "Collar/Sleeve";
+            this.radio_CollarSleeve.UseVisualStyleBackColor = true;
+            // 
+            // radio_Collar
+            // 
+            this.radio_Collar.AutoSize = true;
+            this.radio_Collar.Location = new System.Drawing.Point(21, 15);
+            this.radio_Collar.Name = "radio_Collar";
+            this.radio_Collar.Size = new System.Drawing.Size(59, 16);
+            this.radio_Collar.TabIndex = 1;
+            this.radio_Collar.Text = "Collar";
+            this.radio_Collar.UseVisualStyleBackColor = true;
+            // 
+            // btn_Start
+            // 
+            this.btn_Start.Location = new System.Drawing.Point(519, 15);
+            this.btn_Start.Name = "btn_Start";
+            this.btn_Start.Size = new System.Drawing.Size(75, 23);
+            this.btn_Start.TabIndex = 0;
+            this.btn_Start.Text = "Start";
+            this.btn_Start.UseVisualStyleBackColor = true;
+            this.btn_Start.Click += new System.EventHandler(this.btn_Start_Click);
+            // 
+            // btn_Restart
+            // 
+            this.btn_Restart.Location = new System.Drawing.Point(425, 15);
+            this.btn_Restart.Name = "btn_Restart";
+            this.btn_Restart.Size = new System.Drawing.Size(75, 23);
+            this.btn_Restart.TabIndex = 0;
+            this.btn_Restart.Text = "Restart";
+            this.btn_Restart.UseVisualStyleBackColor = true;
+            this.btn_Restart.Click += new System.EventHandler(this.btn_Restart_Click);
+            // 
+            // FrmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(654, 508);
+            this.Controls.Add(this.pan_SizeRangeType);
+            this.Controls.Add(this.pan_Collar);
+            this.Controls.Add(this.pan_Result);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "FrmMain";
+            this.Text = "Aras Size Range Template V1.0";
+            this.pan_Result.ResumeLayout(false);
+            this.pan_Result.PerformLayout();
+            this.pan_SizeRangeType.ResumeLayout(false);
+            this.pan_SizeRangeType.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel pan_Result;
+        private System.Windows.Forms.TextBox txt_AML;
+        private System.Windows.Forms.Panel pan_Collar;
+        private System.Windows.Forms.Panel pan_SizeRangeType;
+        private System.Windows.Forms.Button btn_Restart;
+        private System.Windows.Forms.RadioButton radio_CollarSleeve;
+        private System.Windows.Forms.RadioButton radio_Collar;
+        private System.Windows.Forms.Button btn_Start;
+    }
+}
+
---
+++ ArasSizeRangeTemplate/FrmMain.cs
@@ -0,0 +1,234 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ArasSizeRangeTemplate
+{
+    public partial class FrmMain : Form
+    {
+        public static string mc_Collar_CM = "36,37,38,39,40,41,42,43,44,45,46,47,48,49,50";
+        public static string mc_Collar_ICON = "13,14,14.5,15,15.5,16,16,16.5,17";
+
+        public static string mc_Sleeve_CM = "79,81,83,87,91,94,97,98";
+        public static string mc_Sleeve_ICON = "13,14,14.5,15,15.5,16,16,16.5,17";
+
+        public static string mc_Collar_prefix = "Collar";
+        public static string mc_Sleeve_prefix = "Sleeve";
+
+        public FrmMain()
+        {
+            InitializeComponent();
+
+            settingControls(false);
+        }
+       
+        public string SettingLevelSpace(int int_level)
+        {
+            StringBuilder l_returnString=new StringBuilder();
+
+            for (int i = 0; i < int_level*4; i++)
+			{
+			    l_returnString.Append(" ");
+			}
+
+            return l_returnString.ToString();
+        }                            
+
+        public void CreateCollar()
+        {
+            string[] l_collar = mc_Collar_CM.Split(',');
+
+            for (int i = 0; i < l_collar.Length; i++)
+            {
+                CheckBox l_chk_Collar = new CheckBox();
+
+                l_chk_Collar.Name = mc_Collar_prefix + "-" + l_collar[i];
+                l_chk_Collar.Text = l_collar[i];
+                l_chk_Collar.Width = 50;               
+
+                l_chk_Collar.Location = new Point(20, 30 * i + 20);
+                l_chk_Collar.Click += new System.EventHandler(this.chk_Collar_Click);
+                pan_Collar.Controls.Add(l_chk_Collar);
+            }
+        }
+
+        public void GetValue()
+        {
+            string[] l_collar = mc_Collar_CM.Split(',');
+            string[] l_sleeve = mc_Sleeve_CM.Split(',');
+
+            StringBuilder l_getAML = new StringBuilder();
+            for (int i = 0; i < l_collar.Length; i++)
+            {
+                #region get collar
+                StringBuilder l_getCollar_AML = new StringBuilder();
+                CheckBox l_chk_Collar = (CheckBox)(pan_Collar.Controls[i]);
+                //CheckBox l_chk_Collar = (CheckBox)l_getCollar_Control[0];
+                if (l_chk_Collar.Checked)
+                {
+                    l_getCollar_AML.Append(SettingLevelSpace(1) + "<Item type=\"Size Range_D1\" action=\"add\">" + "\r\n");
+                    l_getCollar_AML.Append(SettingLevelSpace(2) + "<cn_size>" + l_chk_Collar.Text + "</cn_size>" + "\r\n");
+
+                    #region get sleeve
+
+                    StringBuilder l_getSleeve_AML = new StringBuilder();
+                    Control[] l_getSleevePan = pan_Collar.Controls.Find(l_chk_Collar.Name + "-" + mc_Sleeve_prefix, true);
+
+                    if (l_getSleevePan.Length > 0)
+                    {
+                        Panel l_panSleeve = (Panel)l_getSleevePan[0];
+                        for (int j = 0; j < l_sleeve.Length; j++)
+                        {
+                            CheckBox l_chk_sleeve = (CheckBox)(l_panSleeve.Controls.Find(mc_Sleeve_prefix + "-" + l_sleeve[j], true)[0]);                             
+                            if (l_chk_sleeve.Checked)
+                            {
+                                l_getSleeve_AML.Append(SettingLevelSpace(3) + "<Item type=\"Size Range_D1_D2\" action=\"add\">" + "\r\n");
+                                l_getSleeve_AML.Append(SettingLevelSpace(4) + "<cn_size2>" + l_chk_sleeve.Text + "</cn_size2>" + "\r\n");
+                                l_getSleeve_AML.Append(SettingLevelSpace(3) + "</Item>" + "\r\n");
+                            }
+                        }
+                    }
+
+                    if (l_getSleeve_AML.Length > 0)
+                    {
+                        l_getCollar_AML.Append(SettingLevelSpace(2) + "<Relationships>" + "\r\n");
+                        l_getCollar_AML.Append(l_getSleeve_AML);
+                        l_getCollar_AML.Append(SettingLevelSpace(2) + "</Relationships>" + "\r\n");
+                    }
+
+                    #endregion
+
+                    l_getCollar_AML.Append(SettingLevelSpace(1) + "</Item>" + "\r\n");
+                }
+
+                #endregion
+
+                l_getAML.Append(l_getCollar_AML.ToString());
+            }
+
+            if (l_getAML.Length > 0)
+            {
+                l_getAML.Insert(0, "<Relationships>" + "\r\n");
+
+                l_getAML.Append("<Relationships>" + "\r\n");
+            }
+
+            txt_AML.Text = l_getAML.ToString();
+        }
+
+        //-------------------------------------------------------------
+
+        private void chk_Collar_Click(object sender, EventArgs e)
+        {
+            CheckBox l_chk_Collar = (CheckBox)sender;
+            string[] l_sleeve = mc_Sleeve_CM.Split(',');
+
+            if (l_chk_Collar.Checked)
+            {
+                if (radio_CollarSleeve.Checked)
+                {
+                    Panel l_pan_Sleeve = new Panel();
+
+                    //l_pan_Sleeve.AutoScroll = true;
+                    l_pan_Sleeve.Name = l_chk_Collar.Name + "-" + mc_Sleeve_prefix;
+                    l_pan_Sleeve.Location = new Point(70, l_chk_Collar.Top);
+                    l_pan_Sleeve.BorderStyle = BorderStyle.FixedSingle;
+                    l_pan_Sleeve.Width = 530;
+                    l_pan_Sleeve.Height = 25;
+
+                    for (int i = 0; i < l_sleeve.Length; i++)
+                    {
+                        CheckBox l_chk_Sleeve = new CheckBox();
+
+                        l_chk_Sleeve.Name = mc_Sleeve_prefix + "-" + l_sleeve[i];
+                        l_chk_Sleeve.Text = l_sleeve[i];
+                        l_chk_Sleeve.Width = 40;
+
+                        l_chk_Sleeve.Location = new Point(55 * i + 20, 0);
+                        l_chk_Sleeve.Click += new System.EventHandler(this.chk_Sleeve_Click);
+
+                        l_pan_Sleeve.Controls.Add(l_chk_Sleeve);
+                    }
+
+                    pan_Collar.Controls.Add(l_pan_Sleeve);
+                }
+            }
+            else
+            {
+                Control[] l_pan_sleeve = pan_Collar.Controls.Find(l_chk_Collar.Name + "-" + mc_Sleeve_prefix, true);
+                pan_Collar.Controls.Remove(l_pan_sleeve[0]);
+            }
+
+            GetValue();
+        }
+
+        private void chk_Sleeve_Click(object sender, EventArgs e)
+        {
+            GetValue();
+        }
+
+        public void settingControls(bool bln_Run)
+        {
+            btn_Start.Enabled = !bln_Run;
+            btn_Restart.Enabled = bln_Run;
+
+            if(!bln_Run)
+            {
+                txt_AML.Text = "";
+            }
+        }
+
+        public void showMessage(string pi_message)
+        {
+            MessageBox.Show(pi_message);
+        }
+
+        private void btn_Start_Click(object sender, EventArgs e)
+        {
+            try
+            {                
+
+                pan_Collar.Controls.Clear();
+                CreateCollar();
+
+                settingControls(true);
+            }
+            catch (Exception ex)
+            {
+                showMessage(ex.Message);
+            }
+        }
+
+        private void btn_Restart_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                pan_Collar.Controls.Clear();
+
+                settingControls(false);
+            }
+            catch (Exception ex)
+            {
+                showMessage(ex.Message);
+            }
+        }
+
+        private void txt_AML_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            TextBox textBox = sender as TextBox;
+            if (textBox == null)
+                return;
+            if (e.KeyChar == (char)1)       // Ctrl-A 相当于输入了AscII=1的控制字符
+            {
+                textBox.SelectAll();
+                e.Handled = true;      // 不再发出“噔”的声音
+            }
+        }
+    }
+}
